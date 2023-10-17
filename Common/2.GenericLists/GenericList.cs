fa0d6c076cc34b3faf7817c38e017920d7f10c83
AitorAl <aitoralvarezcu@gmail.com>
2023-10-17 17:27:54 +0200

    hacer cosas

--- Common/2.GenericLists/GenericList.cs
+++ Common/2.GenericLists/GenericList.cs
@@ -38,12 +38,35 @@ public class GenericList<T> : IGenericList<T>
     public void Add(T value)
     {
         //TODO #1: add a new element to the end of the list
+        GenericArrayList<T> currentNode = First;
+        GenericArrayList<T> listNode = new GenericArrayList<T>(value);
+        if (currentNode == null)
+        {
+            First = listNode;
+        }
+        else
+        {
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+            currentNode.Next = listNode;
+        }
     }
 
     public GenericListNode<T> FindNode(int index)
     {
         //TODO #2: Return the element in position 'index'
 
+        int currentPos = 0;
+        GenericArrayList<T> currentNode = First;
+        while (currentPos < index && currentNode != null)
+        {
+            currentNode = currentNode.Next;
+            currentPos++;
+        }
+        if (currentPos == index)
+            return currentNode;
         return null;
     }
 
@@ -51,23 +74,61 @@ public class GenericList<T> : IGenericList<T>
     {
         //TODO #3: return the element on the index-th position. YOU MUST USE GetNode(int). Return the default value for object class T if the position is out of bounds
 
-        return default(T);
+        if (index < 0 || index >= Count())
+        {
+            return 0;
+        }
+        IntListNode x = GetNode(index);
+        return x.Value;
     }
+
     public int Count()
     {
         //TODO #4: return the number of elements on the list
 
-        return 0;
+        int pos = 0;
+        GenericArrayList<T> currentNode = First;
+        if (currentNode != null)
+        {
+            pos++;
+        }
+        else
+        {
+            return pos = 0;
+        }
+
+        while (currentNode.Next != null)
+        {
+            currentNode = currentNode.Next;
+            pos++;
+        }
+        return pos;
     }
 
 
     public void Remove(int index)
     {
         //TODO #5: remove the element on the index-th position. Do nothing if position is out of bounds
+        GenericArrayList<T> currentNode = First;
+
+        if (index == 0 && currentNode != null)
+        {
+            First = currentNode.Next;
+        }
+        while (currentNode.Next != null && index > 1)
+        {
+            currentNode = currentNode.Next;
+            index--;
+        }
+        if (index == 1)
+        {
+            currentNode.Next = currentNode.Next.Next;
+        }
     }
 
     public void Clear()
     {
         //TODO #6: remove all the elements on the list
+        First = null;
     }
 }
