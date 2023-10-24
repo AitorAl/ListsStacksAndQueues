5c989684d18463fdb40f28a30d6a905a323ca414
AitorAl <aitoralvarezcu@gmail.com>
2023-10-24 17:37:12 +0200

    Implementar GenerticQueue y GenerticStack

--- Common/3.StacksAndQueues/GenericQueue.cs
+++ Common/3.StacksAndQueues/GenericQueue.cs
@@ -4,34 +4,37 @@ namespace Common
     public class GenericQueue<T> : IPushPop<T>
     {
         //TODO #1: Declare a List inside this object class to store the objects. Choose the most appropriate object class
-
+        GenericArrayList<T> myList = new GenericArrayList<T>(40000);
         public string AsString()
         {
             //TODO #2: Return the list as a string. Use the method already implemented in your list
-            return null;
+            return myList.AsString();
         }
 
         public int Count()
         {
             //TODO #3: Return the number of objects
-            return 0;
+            return myList.Count();
         }
 
         public void Clear()
         {
             //TODO #4: Remove all objects stored
+            myList.Clear();
         }
 
         public void Push(T value)
         {
             //TODO #5: Add a new object to the list (at the end of it)
+            myList.Add(value); 
         }
 
         public T Pop()
         {
             //TODO #6: Remove the first object of the list and return it
-
-            return default(T);
+            T aux = myList.Get(0);
+            myList.Remove(0);
+            return aux;
         }
     }
 }
--- Common/3.StacksAndQueues/GenericStack.cs
+++ Common/3.StacksAndQueues/GenericStack.cs
@@ -4,34 +4,38 @@ namespace Common
     public class GenericStack<T> : IPushPop<T>
     {
         //TODO #1: Declare a List inside this object class to store the objects. Choose the most appropriate object class
-
+        public GenericArrayList<T> myList = new GenericArrayList<T>(40000);
         public string AsString()
         {
             //TODO #2: Return the list as a string. Use the method already implemented in your list
-            return null;
+            return myList.AsString();
+            
         }
 
         public int Count()
         {
             //TODO #3: Return the number of objects
-            return 0;
+            return myList.Count();
         }
 
         public void Clear()
         {
             //TODO #4: Remove all objects stored
+             myList.Clear();
         }
 
         public void Push(T value)
         {
             //TODO #5: Add a new object to the list (at the end of it)
+             myList.Add(value);
         }
 
         public T Pop()
         {
             //TODO #6: Remove the first object of the list and return it
-
-            return default(T);
+            T aux = myList.Get(myList.Count()-1);
+            myList.Remove(myList.Count() - 1);
+            return aux;
         }
     }
 }
